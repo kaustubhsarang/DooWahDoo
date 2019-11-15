f64247d15c7b669f4c29b016d49a4d7395354909
Abhijeet <force.abhijeet@gmail.com>
2019-11-15 14:36:59 -0600

    userInfoApi

--- src/main/java/com/DooWahDoo/Main/Controller/UserProfileController.java
+++ src/main/java/com/DooWahDoo/Main/Controller/UserProfileController.java
@@ -1,5 +1,25 @@
 package com.DooWahDoo.Main.Controller;
 
-public class UserProfileController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DooWahDoo.Main.Model.UserProfile;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+public class UserProfileController {
+	
+	@Autowired
+	private UserProfileService userProfileService;
+	
+	@RequestMapping("/getUserInfo/userid/{user_id}")
+	public Optional<UserProfile> search(@PathVariable("user_id") long userId) {
+		return userProfileService.getUserDetails(userId);
+	}
 }
